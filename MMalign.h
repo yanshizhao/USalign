e64474f8e5349e8e04acf5d3f014bf26e856515b
yanshizhao <ysz0303@outlook.com>
2026-06-01 17:17:21 +0800

    refactor: TMalign_dimer_main path bool** -> PathMat (Test 4: no new regression)

--- MMalign.h
+++ MMalign.h
@@ -3540,7 +3540,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
     double score_d8,d0,d0_search,dcu0;//for TMscore search
     double t[3], u[3][3]; //Kabsch translation vector and rotation matrix
     DPMatrix score;       // Input score table for dynamic programming
-    bool   **path;        // for dynamic programming
+    PathMat  path;        // for dynamic programming
     DPMatrix val;         // for dynamic programming
     Coords xtm, ytm;      // for TMscore search engine
     Coords xt;            //for saving the superposed version of r_1 or xtm
@@ -3551,7 +3551,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
     /***********************/
     int minlen = min(xlen, ylen);
     score.assign(xlen+1, std::vector<double>(ylen+1));
-    NewArray(&path, xlen+1, ylen+1);
+    path.assign( xlen+1, std::vector<char>(ylen+1));
     val.assign(  xlen+1, std::vector<double>(ylen+1));
     xtm.resize(minlen);
     ytm.resize(minlen);
@@ -3560,6 +3560,8 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
     r2.resize(minlen);
     std::vector<double*> sv(xlen+1), vv(xlen+1);
     for(int _i=0;_i<=xlen;_i++){sv[_i]=score[_i].data();vv[_i]=val[_i].data();}
+    std::vector<char*> pv(xlen+1);
+    for(int _i=0;_i<=xlen;_i++) pv[_i]=path[_i].data();
 
     /***********************/
     //    parameter set   
@@ -3641,7 +3643,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         if (TM>TMmax) TMmax = TM;
         if (TMcut>0) copy_t_u(t, u, t0, u0);
         //run dynamic programing iteratively to find the best alignment
-        TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, path, vv.data(), xa, ya, xlen, ylen,
+        TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya, xlen, ylen,
              mask, t, u, invmap, 0, 2, (fast_opt)?2:30,
              local_d0_search, D0_MIN, Lnorm, d0, score_d8);
         if (TM>TMmax)
@@ -3669,7 +3671,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         /************************************************************/
         //    get initial alignment based on secondary structure   
         /************************************************************/
-        get_initial_ss_dimer(path, vv.data(), secx, secy, xlen, ylen, mask, invmap);
+        get_initial_ss_dimer(reinterpret_cast<bool**>(pv.data()), vv.data(), secx, secy, xlen, ylen, mask, invmap);
         TM = detailed_search(r1, r2, xtm, ytm, xt, xa_c, ya_c, xlen, ylen, invmap,
             t, u, simplify_step, score_sum_method, local_d0_search, Lnorm,
             score_d8, d0);
@@ -3681,7 +3683,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         }
         if (TM > TMmax*0.2)
         {
-            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, path, vv.data(), xa, ya,
+            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya,
                 xlen, ylen, mask, t, u, invmap, 0, 2,
                 (fast_opt)?2:30, local_d0_search, D0_MIN, Lnorm, d0, score_d8);
             if (TM>TMmax)
@@ -3711,7 +3713,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         //    get initial alignment based on local superposition   
         /************************************************************/
         //=initial5 in original TM-align
-        if (get_initial5_dimer( r1, r2, xtm, ytm, path, vv.data(), xa, ya,
+        if (get_initial5_dimer( r1, r2, xtm, ytm, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya,
             xlen, ylen, mask, invmap, d0, d0_search, fast_opt, D0_MIN))
         {
             TM = detailed_search(r1, r2, xtm, ytm, xt, xa_c, ya_c, xlen, ylen,
@@ -3725,7 +3727,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
             }
             if (TM > TMmax*ddcc)
             {
-                TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, path, vv.data(), xa, ya,
+                TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya,
                     xlen, ylen, mask, t, u, invmap, 0, 2, 2,
                     local_d0_search, D0_MIN, Lnorm, d0, score_d8);
                 if (TM>TMmax)
@@ -3758,7 +3760,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         // get initial alignment by local superposition+secondary structure
         /********************************************************************/
         //=initial3 in original TM-align
-        get_initial_ssplus_dimer(r1, r2, sv.data(), path, vv.data(), secx, secy, xa_c, ya_c,
+        get_initial_ssplus_dimer(r1, r2, sv.data(), reinterpret_cast<bool**>(pv.data()), vv.data(), secx, secy, xa_c, ya_c,
             xlen, ylen, mask, invmap0, invmap, D0_MIN, d0);
         TM = detailed_search(r1, r2, xtm, ytm, xt, xa_c, ya_c, xlen, ylen, invmap,
              t, u, simplify_step, score_sum_method, local_d0_search, Lnorm,
@@ -3771,7 +3773,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         }
         if (TM > TMmax*ddcc)
         {
-            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, path, vv.data(), xa, ya,
+            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya,
                 xlen, ylen, mask, t, u, invmap, 0, 2,
                 (fast_opt)?2:30, local_d0_search, D0_MIN, Lnorm, d0, score_d8);
             if (TM>TMmax)
@@ -3814,7 +3816,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
         }
         if (TM > TMmax*ddcc)
         {
-            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, path, vv.data(), xa, ya,
+            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya,
                 xlen, ylen, mask, t, u, invmap, 1, 2, 2,
                 local_d0_search, D0_MIN, Lnorm, d0, score_d8);
             if (TM>TMmax)
@@ -3885,7 +3887,7 @@ inline int TMalign_dimer_main(Coords& xa_c, Coords& ya_c,
                 for (i = 0; i<ylen; i++) invmap0[i] = invmap[i];
             }
             // Different from get_initial, get_initial_ss and get_initial_ssplus
-            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, path, vv.data(), xa, ya,
+            TM = DP_iter_dimer(r1, r2, xtm, ytm, xt, reinterpret_cast<bool**>(pv.data()), vv.data(), xa, ya,
                 xlen, ylen, mask, t, u, invmap, 0, 2,
                 (fast_opt)?2:30, local_d0_search, D0_MIN, Lnorm, d0, score_d8);
             if (TM>TMmax)
