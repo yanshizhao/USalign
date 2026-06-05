12e689a608488485d748ae935e9397687da70e52
yanshizhao <ysz0303@outlook.com>
2026-06-05 23:01:15 +0800

    refactor(P4h): NWDP_TM新增Vec3/RotMat重载(函数体相同, operator[]等价)

--- NW.h
+++ NW.h
@@ -19,6 +19,9 @@ inline void NWDP_TM(const DoubleMatrix& score, CharMatrix& path,
 inline void NWDP_TM(CharMatrix& path, DoubleMatrix& val, const CoordArray& x, const CoordArray& y,
     int len1, int len2, double t[3], double u[3][3],
     double d02, double gap_open, std::vector<int>& j2i);
+inline void NWDP_TM(CharMatrix& path, DoubleMatrix& val, const CoordArray& x, const CoordArray& y,
+    int len1, int len2, const Vec3& t, const RotMat& u,
+    double d02, double gap_open, std::vector<int>& j2i);
 inline void NWDP_TM(CharMatrix& path, DoubleMatrix& val, const char *secx, const char *secy,
     const int len1, const int len2, const double gap_open, std::vector<int>& j2i);
 inline void NWDP_SE(CharMatrix& path, DoubleMatrix& val, CoordArray& x, CoordArray& y,
@@ -81,6 +84,50 @@ inline void NWDP_TM(CharMatrix& path, DoubleMatrix& val, const CoordArray& x, co
         transform(t, u, (double*)&x[i-1][0], xx);
         for(j=1; j<=len2; j++) {
             dij=dist(xx, (double*)&y[j-1][0]);
+            d=val[i-1][j-1] + 1.0/(1+dij/d02);
+            h=val[i-1][j]; if(path[i-1][j]) h += gap_open;
+            v=val[i][j-1]; if(path[i][j-1]) v += gap_open;
+            if(d>=h && d>=v) { path[i][j]=true; val[i][j]=d; }
+            else { path[i][j]=false; if(v>=h) val[i][j]=v; else val[i][j]=h; }
+        }
+    }
+    i=len1; j=len2;
+    while(i>0 && j>0)
+    {
+        if(path[i][j]) //from diagonal
+        {
+            j2i[j-1]=i-1;
+            i--;
+            j--;
+        }
+        else
+        {
+            h=val[i-1][j];
+            if(path[i-1][j]) h +=gap_open;
+
+            v=val[i][j-1];
+            if(path[i][j-1]) v +=gap_open;
+
+            if(v>=h) j--;
+            else i--;
+        }
+    }
+}
+
+// Vec3/RotMat overload (same body)
+inline void NWDP_TM(CharMatrix& path, DoubleMatrix& val, const CoordArray& x, const CoordArray& y,
+    int len1, int len2, const Vec3& t, const RotMat& u,
+    double d02, double gap_open, std::vector<int>& j2i)
+{
+    int i,j; double h,v,d;
+    for(i=0; i<=len1; i++) { val[i][0]=0; path[i][0]=false; }
+    for(j=0; j<=len2; j++) { val[0][j]=0; path[0][j]=false; j2i[j]=-1; }
+    Vec3 xx;
+    double dij;
+    for(i=1; i<=len1; i++) {
+        transform(t, u, x[i-1], xx);
+        for(j=1; j<=len2; j++) {
+            dij=dist(xx, y[j-1]);
             d=val[i-1][j-1] + 1.0/(1+dij/d02);
             h=val[i-1][j]; if(path[i-1][j]) h += gap_open;
             v=val[i][j-1]; if(path[i][j-1]) v += gap_open;
