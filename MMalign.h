50e46381d5b186d806229020300fac1c928bd139
yanshizhao <ysz0303@outlook.com>
2026-06-05 22:45:51 +0800

    refactor(P4g): NWDP_TM_dimer新增Vec3/RotMat重载(函数体相同, operator[]等价)

--- MMalign.h
+++ MMalign.h
@@ -1938,6 +1938,38 @@ inline void NWDP_TM_dimer(CharMatrix& path, DoubleMatrix& val, CoordArray& x, Co
     }
 }
 
+// Vec3/RotMat overload (same body)
+inline void NWDP_TM_dimer(CharMatrix& path, DoubleMatrix& val, CoordArray& x, CoordArray& y,
+    int len1, int len2, CharMatrix& mask, const Vec3& t, const RotMat& u,
+    double d02, double gap_open, std::vector<int>& j2i)
+{
+    int i,j; double h,v,d;
+    for(i=0; i<=len1; i++) { val[i][0]=i*gap_open; path[i][0]=0; }
+    for(j=0; j<=len2; j++) { val[0][j]=j*gap_open; path[0][j]=0; j2i[j]=-1; }
+    Vec3 xx;
+    double dij;
+    for(i=1; i<=len1; i++) {
+        transform(t, u, x[i-1], xx);
+        for(j=1; j<=len2; j++) {
+            d=FLT_MIN;
+            if (mask[i][j]) { dij=dist(xx, y[j-1]); d=val[i-1][j-1] + 1.0/(1+dij/d02); }
+            h=val[i-1][j]; if(path[i-1][j]) h += gap_open;
+            v=val[i][j-1]; if(path[i][j-1]) v += gap_open;
+            if(d>=h && d>=v) { path[i][j]=1; val[i][j]=d; }
+            else { path[i][j]=0; if(v>=h) val[i][j]=v; else val[i][j]=h; }
+        }
+    }
+    i=len1; j=len2;
+    while(i>0 && j>0) {
+        if(path[i][j]) { j2i[j-1]=i-1; i--; j--; }
+        else {
+            h=val[i-1][j]; if(path[i-1][j]) h +=gap_open;
+            v=val[i][j-1]; if(path[i][j-1]) v +=gap_open;
+            if(v>=h) j--; else i--;
+        }
+    }
+}
+
 
 inline void NWDP_TM_dimer(CharMatrix& path, DoubleMatrix& val, const char *secx, const char *secy,
     const int len1, const int len2, CharMatrix& mask, const double gap_open, std::vector<int>& j2i)
